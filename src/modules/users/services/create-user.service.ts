cdefee96ea598c19f4e78d33678710d0267d7a53
Kaue Piovan <kaupiovan@gmail.com>
2026-01-13 23:20:57 -0300

    feat: create-user.service

--- src/modules/users/services/create-user.service.ts
+++ src/modules/users/services/create-user.service.ts
@@ -1,8 +1,11 @@
-import { IcreateUserBody } from "../interfaces/create-user-body";
+import { ICreateUserBody } from "../interfaces/create-user-body";
+import { hash } from "bcryptjs";
 import crypto from "node:crypto";
+import { UsersRepository } from "../repositories/users.repository";
 
 export class CreateUserService {
-  async execute(data: IcreateUserBody) {
+  async execute(data: ICreateUserBody) {
+
     const { name, email, password } = data;
 
     if (password.length < 6)
@@ -10,11 +13,16 @@ export class CreateUserService {
     if (name.length < 3)
       throw new Error("Name must have at least 3 characters");
 
+    const passwordHash = await hash(password, 10);
+
     const user = {
       id: crypto.randomUUID(),
       name,
       email,
-
+      password: passwordHash
     };
+
+    const usersRepository = new UsersRepository();
+    return usersRepository.create(user);
   }
 }
